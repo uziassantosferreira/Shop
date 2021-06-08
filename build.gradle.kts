9d780e10d1f5cff51ac381569b843de9273c7cee
Uzias Ferreira <uziasferreira@gmail.com>
2021-06-07 22:16:55 -0300

    migrate gradle to kts

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath(kotlin("gradle-plugin", version = "1.5.10"))
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
